b5ff90cfd6da769f9622e5f09c620bb4cde53561
zpawel <zpawel92@gmail.com>
2015-08-14 09:40:21 +0200

    exercise2

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -3,7 +3,12 @@
     'use strict';
     function BlogPostCtrl(PostDAO)
     {
-
+        var ctrl = this;
+        var array;
+        PostDAO.query().then(function(data){
+            console.log(data);
+            ctrl.posts = data;
+        })
     }
 
     var module = angular.module('exerciseApp');
