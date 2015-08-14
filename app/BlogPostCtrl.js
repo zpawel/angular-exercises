2a84a057c8aeae21a9fc2a26c3aa1c37d6e674ff
zpawel <zpawel92@gmail.com>
2015-08-14 09:16:14 +0200

    exercise1

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -1,9 +1,14 @@
 (function ()
 {
+
+
+
     'use strict';
     function BlogPostCtrl()
     {
-
+        var ctrl = this;
+        ctrl.posts = [{id: 1, author: 'Jack', title: 'Diving Deep with Dependency Injection'},
+            {id: 2, author: 'Jill', title: 'Practical End-to-End Testing with Protractor'}]
     }
 
     var module = angular.module("exerciseApp", []);
